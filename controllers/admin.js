5b8be80d13200c7d74060f86a5e8cac3d68d1da0
carter1981 <rmisjuk@gmail.com>
2021-03-19 19:44:03 +0300

    Added mvc architecture to the project

---
+++ controllers/admin.js
@@ -0,0 +1,65 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const { title, imageUrl, description, price } = req.body;
+  const product = new Product(null, title, imageUrl, description, price);
+
+  product.save();
+
+  res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  const prodId = req.params.productId;
+
+  if (!editMode) {
+    res.redirect('/');
+  }
+
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      res.redirect('/');
+    }
+
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product,
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const { productId, title, imageUrl, description, price } = req.body;
+  const product = new Product(productId, title, imageUrl, description, price);
+
+  product.save();
+  res.redirect('/admin/products');
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const { productId } = req.body;
+
+  Product.deleteById(productId);
+  res.redirect('/admin/products');
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products',
+    });
+  });
+};
--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,23 @@
-const path = require('path');
-
 const express = require('express');
-
-const rootDir = require('../util/path');
-const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
-});
+const shopController = require('../controllers/shop');
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
+router.get('/checkout', shopController.getCheckout);
+
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
