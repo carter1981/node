65fe153bdac1b950f180421d76672dc9779ba5fa
carter1981 <rmisjuk@gmail.com>
2021-03-22 11:52:39 +0300

    Added sequilize pkg and logic for app using sequilize

--- models/order-item.js
+++ models/order-item.js
@@ -2,13 +2,14 @@ const Sequilize = require('sequelize');
 
 const sequilize = require('../util/database');
 
-const Cart = sequilize.define('cart', {
+const OrderItem = sequilize.define('orderItem', {
   id: {
     type: Sequilize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
   },
+  quantity: Sequilize.INTEGER,
 });
 
-module.exports = Cart;
+module.exports = OrderItem;
